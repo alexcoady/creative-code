37a7756ebed3a508ff746efbc47c269b3fd1e60d
Alex Coady <alexandercoady@me.com>
2014-01-08 00:00:24 +0000

    Added makeshift scroll spy type script

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,82 @@
-console.log('\'Allo \'Allo!');
+var creativecode = function () {
+	'use strict';
+
+	console.log('hi bbz');
+
+	/*
+	*	Loops through page sections and binds to
+	*	their coming in and out of window focus
+	*
+	*	@method scrollSpy
+	*/
+	function scrollSpy () {
+
+		console.log('Initialising scroll spy');
+
+		// Find every page section
+		var $pages = $('.page'),
+			pageCount = $pages.length,
+			i = 0,
+			page,
+			pages = [],
+			windowHeight = $(window).height();
+
+		// Loop through them
+		for ( i; i < pageCount; i += 1 ) {
+
+			var thisPage = $pages[i],
+				position = $(thisPage).position();
+
+			// $pages[i].length;
+			page = {
+				el: thisPage,
+				$el: $(thisPage),
+				upper: position.top,
+				lower: position.top + $(thisPage).height()
+			};
+
+			pages.push(page);
+		}
+
+		// Bind to window scroll
+		function bindScroll () {
+
+			var scrollTop = $(window).scrollTop(),
+				page;
+
+			for ( i = 0; i < pageCount; i += 1 ) {
+
+				page = pages[i];
+
+				if ( scrollTop + (windowHeight/2) >= page.upper && scrollTop + (windowHeight/2) <= page.lower ) {
+
+					console.log( 'active', page.el );
+					
+					page.$el.addClass('is-active been-active');
+				} else {
+
+					page.$el.removeClass('is-active');
+				}
+			}
+		}
+
+		$(window).on('scroll', bindScroll);
+
+		// Apply classes / fire events
+	}
+
+	return {
+
+		scrollSpy: scrollSpy
+	};
+};
+
+// To avoid self-calling above
+var code = creativecode();
+
+
+$(function () {
+	'use strict';
+
+	code.scrollSpy();
+});
